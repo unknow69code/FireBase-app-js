24017ec1b0e213b62b6a74c470b1dc4e347d71cc
unknow69code <anonymus69007@gmail.com>
2023-08-23 16:05:09 -0500

    add Modals

---
+++ src/app/signupformm.js
@@ -0,0 +1,9 @@
+const signupForm = document.querySelector("#signup-form")
+signupForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+  const email = signupForm["signup-email"].value
+  const password = signupForm["signup-password"].value
+
+    console.log(email, password)
+})
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './app/firebase.js'
+import './app/signupformm.js'
 
 console.log("Hello World")
